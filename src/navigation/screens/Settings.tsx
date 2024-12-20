222cc82d9a091a760e838691881e884a0f301e88
Charles van Lennep <charles@eolasmedical.com>
2024-12-20 12:39:08 +0000

    Refresh Control example

--- src/navigation/screens/Settings.tsx
+++ src/navigation/screens/Settings.tsx
@@ -1,10 +1,16 @@
-import { Text } from '@react-navigation/elements';
-import { StyleSheet, View } from 'react-native';
+import { Text } from "@react-navigation/elements";
+import { StyleSheet, View, ScrollView, RefreshControl } from "react-native";
 
 export function Settings() {
   return (
     <View style={styles.container}>
-      <Text>Settings Screen</Text>
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={false} onRefresh={() => {}} />
+        }
+      >
+        <Text style={styles.text}>Empty List</Text>
+      </ScrollView>
     </View>
   );
 }
@@ -12,12 +18,16 @@ export function Settings() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     gap: 10,
   },
   row: {
-    flexDirection: 'row',
+    flexDirection: "row",
     gap: 10,
+  },
+  text: {
+    width: "100%",
+    padding: 10,
   },
 });
